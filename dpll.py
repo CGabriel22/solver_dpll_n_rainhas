568cf6502a471f0ee7080eef5ce34a5b97ab689f
Ícaro <joaoicaromoreira@gmail.com>
2023-07-02 13:15:44 -0300

    Alt Finais por Ícaro e Gabriel

--- dpll.py
+++ dpll.py
@@ -1,17 +1,21 @@
 # TRABALHO FINAL - LÓGICA PARA COMPUTAÇÃO - 2023.1 - 13:30
 # JOAO ICARO MOREIRA, CARLOS GABRIEL LEITE, SIDEVALDO VINICIUS
 
-import solver
+from solver import Solver
 import time
+
 
 starting_time = time.time()
 
-N = 4
+
+N = 64  #altere aqui o tamanho do tabuleiro
 counter = 1
 clauses = []
 mapping_to_int = {}
 mapping_to_int_inv = {}
 positions = [[i, j] for i in range(1, N+1) for j in range(1, N+1)]  # [1, 1] até [N, N]
+
+solver = Solver()
 
 for position in positions:
     key = f"Q_{position[0]}_{position[1]}"
@@ -33,8 +37,7 @@ for j in range(1, N+1):
         other_indexes = list(range(1, N+1))
         other_indexes.remove(i)
         for other in other_indexes:
-            clause = [-mapping_to_int[f"Q_{i}_{j}"], -
-                      mapping_to_int[f"Q_{other}_{j}"]]
+            clause = [-mapping_to_int[f"Q_{i}_{j}"], - mapping_to_int[f"Q_{other}_{j}"]]
             solver.add_clause(clause)
             clauses.append(clause)
 
@@ -44,8 +47,7 @@ for diff in range(2 - N, N - 1):
     a = [[i, j] for i in range(1, N+1) for j in range(1, N+1) if i-j == diff]
     for i in range(len(a)):
         for j in range(i + 1, len(a)):
-            clause = [-mapping_to_int[f"Q_{a[i][0]}_{a[i][1]}"], -
-                      mapping_to_int[f"Q_{a[j][0]}_{a[j][1]}"]]
+            clause = [-mapping_to_int[f"Q_{a[i][0]}_{a[i][1]}"], - mapping_to_int[f"Q_{a[j][0]}_{a[j][1]}"]]
             solver.add_clause(clause)
             clauses.append(clause)
 
@@ -55,14 +57,13 @@ for diff in range(3, N + N):
     a = [[i, j] for i in range(1, N+1) for j in range(1, N+1) if i+j == diff]
     for i in range(len(a)):
         for j in range(i + 1, len(a)):
-            clause = [-mapping_to_int[f"Q_{a[i][0]}_{a[i][1]}"], -
-                      mapping_to_int[f"Q_{a[j][0]}_{a[j][1]}"]]
+            clause = [-mapping_to_int[f"Q_{a[i][0]}_{a[i][1]}"], - mapping_to_int[f"Q_{a[j][0]}_{a[j][1]}"]]
             solver.add_clause(clause)
             clauses.append(clause)
 
 
 def print_board(model):
-    count = 0
+    queens = 0
     board = []
 
     for _ in range(N):
@@ -75,32 +76,32 @@ def print_board(model):
         for j in range(1, N+1):
             if mapping_to_int[f"Q_{i}_{j}"] in model:
                 board[i-1][j-1] = '1'
-                count += 1
+                queens += 1
     for row in board:
         print(' '.join(row))
 
-    return count, board
+    return queens, board
 
-if solver.solve():
+print('\nInformações a respeito do DPLL')
+print('\nClausulas Geradas:',clauses)
+print('\nModelo Gerado:', solver.get_model(), '\n')
 
-    print('\nInformações a respeito do DPLL')
-    print('\nclauses Geradas:',clauses)
-    print('\nModelo Gerado:', solver.get_model(), '\n')
-
+try:
     model = solver.get_model() #gerar o modelo
 
     print('Informações Sobre o Problema:\n')
     print(f'board: {N}x{N}\n')
 
-    rainhas, board = print_board(model) #gerar o numero de rainhas e abstrair o board
-    print(f'\nQuantidade de Rainhas: {rainhas}')
-    
+    queens, board = print_board(model) #gerar o numero de rainhas e abstrair o board
+    print(f'\nQuantidade de Rainhas: {queens}')
+
     ending_time = time.time()
     print(f"Tempo de Execução: {ending_time - starting_time} segundos")
 
     arq = open('entrada.txt', 'w')
-    arq.writelines(f"{rainhas}")
+    arq.writelines(f"{queens}")
     for row in board:
         arq.writelines(f"\n{' '.join(row)}")
-else:
-    print("Sem solução")
+
+except Exception as Erro:
+    print("Tamanho do Tabuleiro UNSAT")
--- solver.py
+++ solver.py
@@ -1,100 +1,77 @@
-solution = []
-clausulas = []
+class Solver:
+    def __init__(self):
+        self.solution, self.clauses = [], []
 
+    def add_clause(self, clause):
+        self.clauses.append(clause)
 
-def add_clause(clausula):
-    clausulas.append(clausula)
-    return clausulas
-
-
-def Simplifica(formula):
-    while True:
-        clausula_unitaria = None
-        for clausula in formula:
-            if len(clausula) == 1:
-                clausula_unitaria = clausula[0]
+    def simplify(self, formula):
+        while True:
+            unit_clause = next((clause[0] for clause in formula if len(clause) == 1), None)
+            if unit_clause is None:
                 break
-        if clausula_unitaria is None:
-            break
-        # print(clausula_unitaria)
-        solution.append(clausula_unitaria)
-        # print(formula)
-        formula = [c for c in formula if clausula_unitaria not in c]
-        # print(formula)
-        formula = [[l for l in c if l != -clausula_unitaria] for c in formula]
-        # print(formula)
-    return formula
+            self.solution.append(unit_clause)
+            formula = [c for c in formula if unit_clause not in c]
+            formula = [[l for l in c if l != -unit_clause] for c in formula]
+        return formula
 
+    def dpll(self, formula):
+        simplified_formula = self.simplify(formula)
 
-def DPLL(formula):
-    formula_simplificada = Simplifica(formula)
+        if not simplified_formula:
+            return True
+        if [] in simplified_formula:
+            return False
 
-    if not formula_simplificada:
-        return True
-    if [] in formula_simplificada:
+        literal = simplified_formula[0][0]
+
+        if self.dpll(simplified_formula + [[literal]]):
+            return True
+        if self.dpll(simplified_formula + [[-literal]]):
+            return True
+
         return False
 
-    literal = formula_simplificada[0][0]
+    @staticmethod
+    def find_literals(lst):
+        if isinstance(lst[0], int):
+            positive_list = [abs(number) for number in lst]
+        else:
+            flattened_list = [element for sublist in lst for element in sublist]
+            positive_list = [abs(number) for number in flattened_list]
 
-    if DPLL(formula_simplificada + [[literal]]):
-        return True
-    if DPLL(formula_simplificada + [[-literal]]):
-        return True
+        no_duplicates_list = list(set(positive_list))
+        return no_duplicates_list
 
-    return False
+    @staticmethod
+    def check_missing_values(l1, l2):
+        missing_values = []
 
+        for value in l1:
+            if value not in l2:
+                missing_values.append(value)
 
-def solve():
-    if DPLL(clausulas):
-        return True
-    else:
-        return False
+        return missing_values
 
+    def get_model(self):
+        result = self.dpll(self.clauses)
+        if not result:
+            return "Insatisfazível!"
+        if self.clauses == []:
+            return "Informe as clausulas!"
+        
+        literals = self.find_literals(self.clauses)
+        solution_literals = self.find_literals(self.solution)
+        missing_values = self.check_missing_values(literals, solution_literals)
 
-def processar_lista(lista):
-    if isinstance(lista[0], int):
-        # Se for uma lista de inteiros simples
-        lista_positivos = [abs(numero) for numero in lista]
-    else:
-        # Se for uma lista de listas
-        lista_unica = [elemento for sublist in lista for elemento in sublist]
-        lista_positivos = [abs(numero) for numero in lista_unica]
+        self.solution += missing_values
+        sorted_list = sorted(self.solution, key=abs)
 
-    lista_sem_repeticoes = list(set(lista_positivos))
-    return lista_sem_repeticoes
+        last_occurrence = {}
 
+        for i in sorted_list:
+            last_occurrence[abs(i)] = i
+            new_list = [last_occurrence[j]
+                        for j in range(1, len(last_occurrence) + 1)]
 
-def verificar_valores_faltantes(l1, l2):
-    valores_faltantes = []
-
-    for valor in l1:
-        if valor not in l2:
-            valores_faltantes.append(-valor)
-
-    return valores_faltantes
-
-
-def montar_solution(solution):
-    literais = processar_lista(clausulas)
-    solutions_literais = processar_lista(solution)
-    valores_faltantes = verificar_valores_faltantes(
-        literais, solutions_literais)
-
-    # print(solution)
-    solution += valores_faltantes
-    lista_ordenada = sorted(solution, key=abs)
-
-    # criar lista com as ultimas ocorrencias
-
-    ultima_ocorrencia = {}
-
-    for i in lista_ordenada:
-        ultima_ocorrencia[abs(i)] = i
-        nova_lista = [ultima_ocorrencia[j]
-                      for j in range(1, len(ultima_ocorrencia) + 1)]
-
-    return nova_lista
-
-
-def get_model():
-    return montar_solution(solution)
+        return new_list
